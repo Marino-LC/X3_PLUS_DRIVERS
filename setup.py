96e981144412e55aab07e1264f043899f27df5f5
LeonardoMendez-FI <leonardo.mendez.fi@gmail.com>
2026-07-30 16:25:36 -0600

    Primer commit

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 from setuptools import find_packages, setup
+from glob import glob
+
 
 package_name = 'rx3_robot_bridge'
 
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+        glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
